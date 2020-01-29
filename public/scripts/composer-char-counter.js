3da44dc7f30d4210c5f8fb002f6086727f6a1919
QuintonEL <quintonlaborde@gmail.com>
2020-01-29 00:35:24 +0000

    created formatting for tweet feed box and hover animations

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -12,25 +12,38 @@ const checker = function(key) {
   }
   return true;
 }
-$(document).ready(function() {
 
-  $(this).on ('keydown', function(key){
+// when the document is rendered and ready to listen go into the function
+//$(document).ready(function() {
+jQuery(function($) {
 
-    if (key.keyCode === 8) { //backspace
-      if (i < 140) {
-        $(".counter").text (i += 1); // increase characters remaining count
-        if (i > 0) {
-          $(".counter").removeClass('color-red'); // remove red if positive
-        }
-      }
+  //$('textarea').on('keydown', function(key){ // this refers to textarea
+  $('.new-tweet').on('keyup', '#tweet textarea', function(key){
+    const charsTyped = $(this).val().length; // get the length of the current value in this
+    const charsLeft = 140 - charsTyped; // get remaining characters left
+    const updatedCount = $(this).next().find('.counter').html(charsLeft); // get html for charsLeft of the counter class in the span 
+
+    if (charsLeft < 0) {
+      updatedCount.addClass('color-red');
     } else {
-      let res = checker(key.keyCode);
-      if (res) {
-        $(".counter").text (i -= 1); // decrease characters remaining count
-        if (i < 0) {
-          $(".counter").addClass('color-red'); // add red if negative
-        }
-      }
+      $('.counter').removeClass('color-red');
     }
+
+    // if (key.keyCode === 8) { //backspace
+    //   if (i < 140) {
+    //     $(".counter").text (i += 1); // increase characters remaining count
+    //     if (i > 0) {
+    //       $(".counter").removeClass('color-red'); // remove red if positive
+    //     }
+    //   }
+    // } else {
+    //   let res = checker(key.keyCode);
+    //   if (res) {
+    //     $(".counter").text (i -= 1); // decrease characters remaining count
+    //     if (i < 0) {
+    //       $(".counter").addClass('color-red'); // add red if negative
+    //     }
+    //   }
+    // }
   });
 });
